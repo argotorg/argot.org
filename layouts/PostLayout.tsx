3d0cd6683da6f13b2b928ecb82559a5233cf33ea
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-04-28 14:34:23 +0200

    Fix blog post colors and others

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -72,7 +72,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       )}
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
-                        <dd className=" ">{author.name}</dd>
+                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
                         <dt className="sr-only">Twitter</dt>
                         <dd>
                           {author.twitter && (
