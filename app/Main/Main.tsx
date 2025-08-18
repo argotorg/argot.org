3b482fb8db7aaafed2edcbf0429e0a3f97727ee0
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-08-18 15:20:22 +0200

    Change subtext and add about section

--- app/Main/Main.tsx
+++ app/Main/Main.tsx
@@ -2,6 +2,7 @@ import ProjectCard from './components/ProjectCard'
 import { projects } from './data/projects'
 import AnnouncementBanner from './components/AnnouncementBanner'
 import Hero from './components/Hero'
+import About from './components/About'
 
 export default function Main() {
   return (
@@ -9,17 +10,23 @@ export default function Main() {
       <Hero />
 
       <section className="mx-auto max-w-3xl space-y-4 py-12 text-sm md:text-xl">
-        <p className="mx-auto max-w-prose text-center">
-          The Argot Collective is a new non-profit organization maintaining Ethereum's core
-          infrastructure formed by 25 former Ethereum Foundation employees.
-          <br /> <br />
-          Operating democratically and transparently, we aim to provide stable, long-term support
-          for crucial projects, free from commercial pressures.
-        </p>
+        <div className="rounded-lg bg-ecru-300 p-8 dark:bg-anthracite-700">
+          <p className="mx-auto max-w-prose text-justify">
+            Argot Collective is a non-profit, independent research and development group sustaining
+            Ethereum's core programming languages and tooling, most notably Solidity.
+            <br />
+            <br />
+            By uniting expertise in compilers, formal verification, and language design under one
+            roof, we make smart contract and Ethereum application development simpler, safer, and
+            more resilient.
+          </p>
+        </div>
       </section>
 
-      <section className="space-y-4 pb-12">
-        <h2 className="text-center text-3xl font-extrabold md:text-left">Our Projects</h2>
+      <About />
+
+      <section className="space-y-4 py-12">
+        <h2 className="text-center text-3xl font-extrabold">Our Projects</h2>
         <div className="mt-16 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {projects
             .sort((a, b) => a.title.localeCompare(b.title))
