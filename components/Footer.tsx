e7f84cc58e0f5705f43fd8ff1d247b66bdfae79e
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-08-18 18:59:44 +0200

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,18 +9,27 @@ const Footer = () => {
         <div className="flex flex-col items-center space-y-4 py-4 dark:border-gray-700 md:flex-row md:items-center md:justify-between md:space-y-0">
           <div className="flex flex-col items-center space-y-2 text-xl font-medium text-anthracite-700 dark:text-ecru-300 md:flex-row md:space-x-4 md:space-y-0">
             <span className="font-bold md:mr-8">ARGOT © {new Date().getFullYear()}</span>
-            <Link className="link-underline" href="/imprint">
-              Privacy policy
-            </Link>
-            <Link className="link-underline" href="/brand">
-              Brand assets
-            </Link>
           </div>
-          <div className="flex items-center space-x-1">
-            <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={5} />
-            <SocialIcon kind="github" href={siteMetadata.github} size={5} />
-            <SocialIcon kind="farcaster" href={siteMetadata.farcaster} size={5} />
-            <SocialIcon kind="x" href={siteMetadata.x} size={5} />
+          <div className="flex flex-col items-center gap-4 md:flex-row">
+            <div className="flex space-x-4 text-lg font-medium ">
+              <Link className="link-underline" href="/privacy-policy">
+                Privacy Policy
+              </Link>
+              <Link
+                className="link-underline"
+                href="https://github.com/argotorg/assets"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Brand Assets
+              </Link>
+            </div>
+            <div className="flex  space-x-1">
+              <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={5} />
+              <SocialIcon kind="github" href={siteMetadata.github} size={5} />
+              <SocialIcon kind="farcaster" href={siteMetadata.farcaster} size={5} />
+              <SocialIcon kind="x" href={siteMetadata.x} size={5} />
+            </div>
           </div>
         </div>
       </div>
